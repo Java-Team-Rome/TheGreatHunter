3d29148f4b589549d2fb910db0056044d83afe8f
mbeleva88 <m_d_b@mail.bg>
2016-04-05 23:11:23 +0300

    Created Display.class, GameEngine  and Launcher

---
+++ TheGreatHunter/src/com/company/game/GameEngine.java
@@ -0,0 +1,91 @@
+package com.company.game;
+
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import com.company.graphics.Display;
+
+public class GameEngine implements Runnable {
+
+	private String title;
+    private boolean isRunning = false;
+    private Display display;
+    private Thread thread;
+    private BufferStrategy bufferStrategy;
+    private Graphics graphics;
+    private int x;
+    
+    BufferedImage background;
+    
+    public GameEngine(String title) {
+    	this.title = title;
+    }
+    
+	public synchronized void start() {
+        if (isRunning) {
+            return;
+        }
+        
+        this.isRunning = true;
+        this.thread = new Thread(this);
+        thread.start();
+    }
+
+	public synchronized void stop() {
+        if (!isRunning) {
+            return;
+        }
+
+        this.isRunning = false;
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	@Override
+	public void run() {
+		this.init();
+
+        while (isRunning) {
+           this.tick();
+           this.render();
+        }
+
+        this.stop();
+	}
+	
+	private void tick() {
+		x++;
+	}
+	
+	private void render() {
+		this.bufferStrategy = this.display.getCanvas().getBufferStrategy();
+		
+		if (this.bufferStrategy == null) {
+			this.display.getCanvas().createBufferStrategy(2);
+			return;
+		}
+		
+		this.graphics = this.bufferStrategy.getDrawGraphics();
+		
+		// -> START DROWING
+		graphics.fillRect(100, 100, 100, 100);
+		
+		
+		// -> END DROWING
+		
+		this.graphics.dispose();
+		this.bufferStrategy.show();
+	}
+	
+	private void init() {
+
+        this.display = new Display(this.title, 800, 600);
+        
+        this.x = 50;
+    }
+}
---
+++ TheGreatHunter/src/com/company/game/Launcher.java
@@ -0,0 +1,11 @@
+package com.company.game;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		 GameEngine game = new GameEngine("The Great Hunter");
+
+	        game.start();
+	}
+
+}
