afecd73b9da31205789314218bc693413df95d5c
Primo123 <darksoul_666@abv.bg>
2016-04-07 16:18:08 +0300

    Added files via upload

---
+++ TheGreatHunter/src/com/company/models/Inventory.java
@@ -0,0 +1,35 @@
+package com.company.models;
+
+/**
+ * Created by Stark on 4/7/2016.
+ */
+public class Inventory {
+
+    private int meat;
+    private int feather;
+    private int leather;
+
+    public int getMeat() {
+        return meat;
+    }
+
+    public void setMeat(int meat) {
+        this.meat = meat;
+    }
+
+    public int getFeather() {
+        return feather;
+    }
+
+    public void setFeather(int feather) {
+        this.feather = feather;
+    }
+
+    public int getLeather() {
+        return leather;
+    }
+
+    public void setLeather(int leather) {
+        this.leather = leather;
+    }
+}
