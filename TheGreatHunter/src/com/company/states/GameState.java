f3b0259003b0d13da365df79f4211939402494e6
Kristian <kristian.stanimirov@gmail.com>
2016-04-16 23:07:11 +0300

    Add Timer

--- TheGreatHunter/src/com/company/states/GameState.java
+++ TheGreatHunter/src/com/company/states/GameState.java
@@ -1,5 +1,7 @@
 package com.company.states;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import com.company.game.MapInitializer;
@@ -9,30 +11,52 @@ import com.company.models.prey.Prey;
 public class GameState extends State {
 
 	private Prey prey;
-    private int timer;
-    
-    public GameState() {
-    	prey = MapInitializer.generatePray();
+	private int timer;
+	private int seconds = 30;
+	private String secondsPrint;
+
+	public GameState() {
+		prey = MapInitializer.generatePray();
 	}
-    
+
 	@Override
 	public void display(Graphics graphics) {
 		graphics.drawImage(Assets.background, 0, 0, 800, 600, null);
-		
+
 		if (prey.isAlive() && !prey.isHasEscaped()) {
-    		prey.display(graphics);
+			prey.display(graphics);
 		}
+
+		Font secondsFont = new Font("Comic Sans MS", Font.BOLD, 30);
+		graphics.setFont(secondsFont);
+		graphics.setColor(Color.white);
+		graphics.drawString(secondsPrint, 750, 50);
+
 	}
 
 	@Override
 	public void update() {
 		timer++;
-    	if (timer == 3) {
-    		prey.isHasEscaped();
-    		prey = MapInitializer.generatePray();
-    		timer = 0;
+		if (timer == 3) {
+			prey.isHasEscaped();
+			prey = MapInitializer.generatePray();
+			timer = 0;
 		}
-    	
-    	prey.update();
+
+		prey.update();
+
+		seconds--;
+		if (seconds == 0) {
+			System.exit(1);
+		}
+		try {
+			Thread.sleep(900);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		secondsPrint = Integer.toString(seconds);
 	}
+
 }
